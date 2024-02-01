4790dc9f871236965c06cc4a53863e3c379d5386
milenaangelova1 <mangelova@mail.com>
2024-02-01 12:52:28 +0000

    new results for S1

--- src/EdgeCluster.py
+++ src/EdgeCluster.py
@@ -42,29 +42,29 @@ class EdgeCluster:
             update_initial_clustering(new_cluster, initial_clustering)
             cluster = temp_cluster
             
-            # temp_clustering = initial_clustering.copy()
-            # temp_clustering = remove_cluster_from_clustering(temp_clustering, cluster_for_removing = new_cluster)
+            temp_clustering = initial_clustering.copy()
+            temp_clustering = remove_cluster_from_clustering(temp_clustering, cluster_for_removing = new_cluster)
             
             # while True:
-            # closed_cluster = find_the_closest_cluster(new_cluster, temp_clustering)
+            closed_cluster = find_the_closest_cluster(new_cluster, temp_clustering)
 
-            # if closed_cluster is not None:
+            if closed_cluster is not None:
 
-            #     # if not bool(closed_cluster):
-            #     #     break
+                # if not bool(closed_cluster):
+                #     break
 
-            #     if new_cluster['cluster'] != closed_cluster['cluster'] and ((dist(new_cluster['low'], closed_cluster['mean']) >= dist(new_cluster['mean'], closed_cluster['mean'])) or (dist(new_cluster['mean'], closed_cluster['mean']) <= dist(closed_cluster['mean'], closed_cluster['high']))):
-            #         # ((D(l_i, m_j ) >= D(m_i, m_j )) or (D(m_i, m_j ) <= D(m_j , h_j))
-            #         # merge clusters c and j. Merging will be union
-            #         new_next_cluster = recalculate_cluster_params(new_cluster, closed_cluster)
-            #         remove_closed_cluster_metrics(closed_cluster, initial_clustering)
-            #         # find and change the cluster into initial clustering solution
-            #         update_initial_clustering(new_next_cluster, initial_clustering)
-            #         merges.append(closed_cluster['cluster'])
-            #     else:
-            #         merge_with_window = True
-            # else:
-            merge_with_window = True
+                if new_cluster['cluster'] != closed_cluster['cluster'] and ((dist(new_cluster['low'], closed_cluster['mean']) >= dist(new_cluster['mean'], closed_cluster['mean'])) or (dist(new_cluster['mean'], closed_cluster['mean']) <= dist(closed_cluster['mean'], closed_cluster['high']))):
+                    # ((D(l_i, m_j ) >= D(m_i, m_j )) or (D(m_i, m_j ) <= D(m_j , h_j))
+                    # merge clusters c and j. Merging will be union
+                    new_next_cluster = recalculate_cluster_params(new_cluster, closed_cluster)
+                    remove_closed_cluster_metrics(closed_cluster, initial_clustering)
+                    # find and change the cluster into initial clustering solution
+                    update_initial_clustering(new_next_cluster, initial_clustering)
+                    merges.append(closed_cluster['cluster'])
+                else:
+                    merge_with_window = True
+            else:
+                merge_with_window = True
 
         elif (dist(cluster['low'], cluster['mean']) < dist(cluster['mean'], window['mean'])) and (dist(cluster['mean'], window['mean']) > dist(window['mean'], window['high'])):
             # (D(l_i, m_i) < D(m_i, m_w)) and (D(m_i, m_w) > D(m_w, h_w))
--- src/run.py
+++ src/run.py
@@ -245,7 +245,7 @@ def experiment_ampds2_data(hour, type, num_segments: int, batch_size: int, metri
 
 def experiment1(type='continous'):
     size_windows = [3,4,6,8,12,24,30,32,48]   # number of samples in each window
-    size_windows = [48]
+    # size_windows = [24]
     start_time = time.time()
 
     # Experiment with S1 data
@@ -316,12 +316,12 @@ def experiment2():
 
 
 if __name__ == '__main__':
-    # experiment1(type='original')
+    experiment1(type='original')
     # experiment1(type='original_previous')
     # experiment1(type='continuous')
     # experiment1(type='continuous_previous')
 
-    experiment2()
+    # experiment2()
 
     # Experiment with synthetic data
     # Experiment 3-streams with 2-dimensional data
